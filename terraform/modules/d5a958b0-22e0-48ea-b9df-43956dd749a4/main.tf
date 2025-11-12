225ff0fd05e5bd9373671a7e66e019e390b2ec8e
Nehul283 <nehul283.rejoice@gmail.com>
2025-11-12 15:01:26 +0530

    remove unused pool

--- terraform/modules/d5a958b0-22e0-48ea-b9df-43956dd749a4/main.tf
+++
@@ -1,9 +0,0 @@
-
-      terraform {
-        required_version = ">= 1.0.0"
-      }
-
-      # Example for tenant 123
-      # Add your actual terraform logic here
-      2025-11-11T10:36:15.061Z
-      
