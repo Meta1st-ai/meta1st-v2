343e52a984331aba256da67d30178b28aae3998b
Nehul283 <nehul283.rejoice@gmail.com>
2025-11-12 12:18:16 +0530

    chore: add Cognito user pool module for Rejoice-Organisation (user_pool_rejoice_organisation)

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+
+variable "user_pool_rejoice_organisation_name" {
+    description = "Pool name for Rejoice-Organisation"
+    type        = string
+  }
+
+variable "user_pool_rejoice_organisation_azure_metadata_url" {
+    description = "Azure metadata url for Rejoice-Organisation"
+    type        = string
+    default     = ""
+  }
+
+variable "user_pool_rejoice_organisation_azure_tenant_id" {
+    description = "Azure tenant id for Rejoice-Organisation"
+    type        = string
+    default     = ""
+  }
